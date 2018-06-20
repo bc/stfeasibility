4d8a51be6e0e45eb92ab130b0cd6c1e1f7c68c1c
Brian Cohn <brian.cohn@usc.edu>
2018-06-20 15:00:03 -0700

    Closes #1 (#3)

---
+++ R/helper_functions.r
@@ -0,0 +1,32 @@
+constraint_H_rhs_b <- function(A, b) {
+    constr <- list(constr = rbind(A, -A), dir = rep("<=", 2 * nrow(A)), rhs = c(b,
+        -b))
+    return(constr)
+}
+
+col_blank <- function(df, FUN) apply(df, 2, FUN)
+colMaxes <- function(df) col_blank(df, max)
+colMins <- function(df) col_blank(df, min)
+colMedians <- function(df) col_blank(df, median)
+
+constraint_H_with_bounds <- function(A, b, bounds_tuple_of_numeric) {
+    H_constraint <- constraint_H_rhs_b(A, b)
+    bounds <- bound_constraints_for_all_muscles(bounds_tuple_of_numeric)
+    return(mergeConstraints(H_constraint, bounds))
+}
+
+har_sample <- function(constr, n_samples, thin) {
+    state <- har.init(constr, thin = 100)
+    samples <- har.run(state, n.samples = n_samples)$samples %>% as.data.frame
+    return(samples)
+}
+
+
+bound_constraints_for_all_muscles <- function(bounds_tuple_of_numeric) {
+    n_muscles <- length(bounds_tuple_of_numeric)
+    lapply(1:n_muscles, function(muscle_index) {
+        lb <- lowerBoundConstraint(n_muscles, muscle_index, bounds_tuple_of_numeric[[muscle_index]]$lower)
+        ub <- upperBoundConstraint(n_muscles, muscle_index, bounds_tuple_of_numeric[[muscle_index]]$upper)
+        return(mergeConstraints(lb, ub))
+    }) %>% mergeConstraints
+}
---
+++ R/main.r
@@ -0,0 +1,3 @@
+load_all("~/Documents/GitHub/bc/frontiers2017")
+source('R/helper_functions.r')
+la <- load_all
