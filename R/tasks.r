b5c79fb449b0770edf042a8d8787e657631ae9cd
Brian Cohn <brian.cohn@usc.edu>
2020-04-11 22:46:22 -0700

    fixed up some of the ffs functionality

--- R/tasks.r
+++ R/tasks.r
@@ -162,14 +162,36 @@ generate_tasks_and_corresponding_constraints_via_df <- function(H_matrix_input, 
     return(list(tasks = tasks, constraints = list_of_constraints_per_task))
 }
 
+gen_7d_ffs <- function(H_matrix_input){
+    all_combinations <- expand.grid(c(0,1),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1))
+    vals <- as.matrix(H_matrix_input) %*% t(all_combinations) %>% t
+    colnames(vals) <- rownames(H_matrix_input)
+    return(vals%>%data.table)
+}
 
+
+plot_ffs <- function(ffs_points, tasks_df){
+    p <- ggplot(ffs_points, aes(Fx,Fz,col=Fy)) 
+    p <- p + geom_point()
+    p <- p + coord_fixed() 
+    p <- p + theme_bw() 
+    p <- p + geom_hline(yintercept = 0) 
+    p <- p + geom_vline(xintercept = 0)
+    p <- p + stat_chull(alpha = 0.01, geom = "polygon")
+    p <- p + geom_path(data = tasks_df, aes(x=Fx, y = Fz), col="green")
+    ggsave("figures/ffs.pdf")
+}
     st_with_vel <- function(discrete_speed_limit,har_n) {
         loop_update(discrete_speed_limit)
-        st_constr_str <- force_cos_ramp_constraint(H_matrix, bounds_tuple_of_numeric, c(5,0,0,0), discrete_speed_limit, discrete_speed_limit, n_task_values = 7, cycles_per_second=10, eliminate = FALSE)
+        my_H_matrix <- read.csv("/Users/Olive/Downloads/redirection_task/fvc_hentz_2002.csv", row.names=1) %>% as.matrix
+        my_H_matrix <- my_H_matrix
+        ffs_points <- gen_7d_ffs(my_H_matrix)
+        plot_ffs(ffs_points, st_constr_str$tasks_and_constraints$tasks * 10)
+        # H_matrix
+        st_constr_str <- force_cos_ramp_constraint(my_H_matrix, bounds_tuple_of_numeric, c(10,0,0,0), discrete_speed_limit, discrete_speed_limit, n_task_values = 7, cycles_per_second=10, eliminate = FALSE)
         extremes <- lapply(st_constr_str$tasks_and_constraints$constraints, findExtremePoints)
         res <- st_constr_str$nonredundant_constr %>% eliminate_redundant(7)
         points <- res %>% pb_har_sample(har_n, mc.cores=8, eliminate=FALSE)
-        print(summary(points))
         tall_df_st <- points %>% trajectory_har_df_melt(7)
         tall_df_st$velocity_limit <- discrete_speed_limit
         tall_df_st$velocity_limit <- as.character(tall_df_st$velocity_limit)
