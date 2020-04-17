6b5151408e66112b4cabfde902075ca292d0a0d3
Brian Cohn <brian.cohn@usc.edu>
2020-04-16 21:21:28 -0700

    #23 collecting 10k har points per 10 seed solutions

--- tests/testthat/test_seedpoints.r
+++ tests/testthat/test_seedpoints.r
@@ -5,7 +5,7 @@ test_that('we can extract 100 seeds for a given speed multiconstraint #23', {
     #speed is fixed across this entire run below; is dependent on the rda used
     st_res <- readRDS("/Volumes/GoogleDrive/My\ Drive/outputs/ste_1e5_speed_13_timefin_09:04:05.556.rda")
     H_multiconstraint <- attr(st_res, "constraints_and_tasks")$nonredundant_constr
-    activation_per_seed <- extract_n_seeds_from_rda_ste(st_res, 100)
+    activation_per_seed <- extract_n_seeds_from_rda_ste(st_res, 10)
     multiconstraint_per_seed <- lapply(seq(1,ncol(activation_per_seed)), function(task_0_seed_activation){
     	# trim top which has task0 wrench requirements
     	seed_a <- activation_per_seed[,task_0_seed_activation]
@@ -17,21 +17,11 @@ test_that('we can extract 100 seeds for a given speed multiconstraint #23', {
     	})
 
 
-    pbmclapply(multiconstraint_per_seed, function(constraint_with_seed_fixation){
-    	points <- har_sample(constraint_with_seed_fixation, n_samples=1e1, eliminate=TRUE)
-    	attr(points, "constraint_with_seed") <- constraint_with_seed_fixation
-    	seed_id <- attr(constraint_with_seed_fixation, "seed_id")
-    	target_filepath <- sprintf("/Volumes/GoogleDrive/My\ Drive/outputs/seed_evals/%s.rda",seed_id)
-    	saveRDS(points, target_filepath)
-    	})
+    result_filepaths <- pbmclapply(multiconstraint_per_seed, seed_sample_and_save)
 
-        ex <- multiconstraint_per_seed[[44]]
-    data(mtcars)
+       
 seeded_points <- ex%>% har_sample(1000)
-parcoords(seeded_points, reorderable = TRUE, brushMode = "1D-axes-multi", autoresize=TRUE, width=1900, height=500,alpha=0.1)
-
-
-
+	parcoords(seeded_points, reorderable = TRUE, brushMode = "1D-axes-multi", autoresize=TRUE, width=1900, height=500, alpha=0.1)
     })
 
 
