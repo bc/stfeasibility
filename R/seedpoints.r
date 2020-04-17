ecaa8af0b851db56b116b6028cf2e57ed589e5c8
Brian Cohn <brian.cohn@usc.edu>
2020-04-16 18:47:24 -0700

    #23 better seed id generation

--- R/seedpoints.r
+++ R/seedpoints.r
@@ -1,5 +1,6 @@
+require(data.table)
 extract_n_seeds_from_rda_ste <- function(st_res, n){
-	library(data.table)
+	require(data.table)
     seeds <- sample(1:max(st_res$muscle_trajectory),n_seeds,replace=FALSE)
     st_res_dt <- data.table(st_res)
     only_seeds <- st_res_dt[muscle_trajectory%in%seeds & task_index == 0,activation,by=.(muscle_trajectory, muscle)]    
@@ -12,10 +13,12 @@ extract_n_seeds_from_rda_ste <- function(st_res, n){
 }
 
 #puts a diag and -diag equality with the activations in task_0 for the input activations7
-assemble_equality_with_seed_point <- function(activations7){
+assemble_equality_with_seed_point <- function(seed_id, activations7){
 	library(data.table)
 	equalityconst <- cbind(diag(7), matrix(0,7,42))
-	rownames(equalityconst) <- paste0(seeds[,1],"_task0_equality_w_seed_const")
+	rownames(equalityconst) <- rep(paste0(seed_id,"_task0_equality_w_seed_const"), nrow(equalityconst))
+	print('names are')
+	print(equalityconst)
 	colnames(equalityconst) <- colnames(H_multiconstraint$const)
 	equalit_constr_formatted <- create_equality_constraint(equalityconst, activations7)
 	return(list(constr = equalit_constr_formatted$constr[1:7,], dir= rep("=",7),rhs = equalit_constr_formatted$rhs[1:7]))
@@ -23,8 +26,8 @@ assemble_equality_with_seed_point <- function(activations7){
 
 trim_top_of_constraint <- function(constraint, nrows_to_rm){
 	newversion <- constraint # mk copy
-	newversion$constr <- constraint$constr[(nrows_to_rm+1):nrow(ex$constr),]
-	newversion$dir <- constraint$dir[(nrows_to_rm+1):length(ex$dir)]
-	newversion$rhs <- constraint$rhs[(nrows_to_rm+1):length(ex$rhs)]
+	newversion$constr <- constraint$constr[(nrows_to_rm+1):nrow(constraint$constr),]
+	newversion$dir <- constraint$dir[(nrows_to_rm+1):length(constraint$dir)]
+	newversion$rhs <- constraint$rhs[(nrows_to_rm+1):length(constraint$rhs)]
 	return(newversion)
 }
--- tests/testthat/test_seedpoints.r
+++ tests/testthat/test_seedpoints.r
@@ -6,22 +6,26 @@ test_that('we can extract 100 seeds for a given speed multiconstraint #23', {
     st_res <- readRDS("/Volumes/GoogleDrive/My\ Drive/outputs/ste_1e5_speed_13_timefin_09:04:05.556.rda")
     H_multiconstraint <- attr(st_res, "constraints_and_tasks")$nonredundant_constr
     activation_per_seed <- extract_n_seeds_from_rda_ste(st_res, 100)
-    multiconstraint_per_seed <- pbapply(activation_per_seed, 2, function(task_0_seed_activation){
+    multiconstraint_per_seed <- lapply(seq(1,ncol(activation_per_seed)), function(task_0_seed_activation){
     	# trim top which has task0 wrench requirements
-    	res <- merge_constraints(trim_top_of_constraint(H_multiconstraint,22),assemble_equality_with_seed_point(task_0_seed_activation))
-    	attr(res, "seed_activation") <- task_0_seed_activation
+    	seed_a <- activation_per_seed[,task_0_seed_activation]
+    	seed_id <- colnames(activation_per_seed)[task_0_seed_activation]
+    	res <- merge_constraints(trim_top_of_constraint(H_multiconstraint,22),assemble_equality_with_seed_point(seed_id, seed_a))
+    	attr(res, "seed_activation") <- seed_a
+    	attr(res, "seed_id") <- seed_id
     	return(res)
     	})
 
 
-    pbmclapply(multiconstraint_per_seed, function(constraint_with_seed_fixation){
-    	points <- har_sample(constraint_with_seed_fixation, n_samples=1e5, eliminate=TRUE)
+    lapply(multiconstraint_per_seed, function(constraint_with_seed_fixation){
+    	points <- har_sample(constraint_with_seed_fixation, n_samples=1e1, eliminate=TRUE)
     	attr(points, "constraint_with_seed") <- constraint_with_seed_fixation
     	seed_id <- attr(constraint_with_seed_fixation, "seed_activation")
     	attr(points, "seed_activation") <- seed_id
+    	browser()
     	target_filepath <- sprintf("/Volumes/GoogleDrive/My\ Drive/outputs/seed_evals/%s.rda",colnames(seed_id)[1])
     	saveRDS(points, target_filepath)
-    	}, mc.cores=8)
+    	})
 
 
         ex <- multiconstraint_per_seed[[44]]
